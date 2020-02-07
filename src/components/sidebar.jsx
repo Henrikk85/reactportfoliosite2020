c3b5dd60932d3ef8a7fbf55fa6c2f3e1e6995045
Henrik Krieger <henrikkriiger@gmail.com>
2020-02-07 22:35:50 +0200

    Sidebar home link added!

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -30,6 +30,7 @@ export default class Sidebar extends Component {
             </nav>
             <nav id="colorlib-main-menu">
               <ul>
+               <li><a href="http://funktech.ee" target="_blank" rel="noopener noreferrer"><i className="icon-home"></i></a></li> 
                <li><a href="https://www.facebook.com/henrik.krieger.5" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
                <li><a href="https://twitter.com/Henrikk31" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
                <li><a href="https://www.instagram.com/kriegerhenrik/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
